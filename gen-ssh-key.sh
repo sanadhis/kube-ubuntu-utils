fc6c68fc26ecf49388efde2fdd1f7f0b978d4be6
sanadhi <sanadhi@sanadhi.epfl.ch>
2018-03-08 15:48:15 +0100

    --Gen SSH Fix

--- gen-ssh-key.sh
+++ gen-ssh-key.sh
@@ -13,13 +13,12 @@ function main () {
     print-banner "creating keygen on default location"
     ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -q -N ""
     print-banner "adding ssh key to own self"
-    PUBKEY=$(cat ~/.ssh/id_rsa.pub); grep -q "$PUBKEY"  ~/.ssh/authorized_keys || echo "$PUBKEY" >> ~/.ssh/authorized_keys
+    touch $HOME/.ssh/authorized_keys
+    PUBKEY=$(cat $HOME/.ssh/id_rsa.pub); grep -q "$PUBKEY" $HOME/.ssh/authorized_keys || echo "$PUBKEY" >> $HOME/.ssh/authorized_keys
 }
 
-if  [ "$UID" -e 0 ] ; then
+if  [ "$UID" -eq 0 ] ; then
     echo "Please run as non-root"
 else
-    ensure-dependencies
-    add-influxdb-repo-conf
     main "$@"
 fi
